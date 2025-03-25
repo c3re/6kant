e1f347280955665b6ecd5200b3eb95ba40cf5e10
Dome-Maurice <75705845+Dome-Maurice@users.noreply.github.com>
2025-03-25 20:29:29 +0100

    changed Pin Defs

--- src/configuration.hpp
+++ src/configuration.hpp
@@ -1,21 +1,22 @@
 #pragma once
 
-#define BUZZER_PIN 3
+#define BUZZER_PIN 18
 
-#define BTN1_PIN 12
+#define BTN1_PIN 4
 #define BTN2_PIN 13
 #define BTN3_PIN 14
-#define BTN4_PIN 15
-#define BTN5_PIN 16
-#define BTN6_PIN 17
+#define BTN4_PIN 26
+#define BTN5_PIN 32
+#define BTN6_PIN 33
 
-#define GAME_FIELD_LED_DATA_PIN 18
-#define GAME_BUTTONS_LED_DATA_PIN 19
+#define GAME_FIELD_LED_DATA_PIN 16
+#define GAME_BUTTONS_LED_DATA_PIN 17
 
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define NUM_GAME_FIELD_LEDS 60
-#define BRIGHTNESS  255
+#define BRIGHTNESS  200
+#define MAX_CURRENT 2000 //mA
 
 #define PLAYER1_COLOR CRGB(255, 0, 0) //OR 0, 51, 102 ?
 #define PLAYER2_COLOR CRGB(0, 255, 0) //OR 0, 102, 51
